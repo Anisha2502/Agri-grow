b713ab776744768d65c00aeecd400e2f8e7e4708
Anisha Wagh <anishawagh2@gmail.com>
2025-07-25 02:21:11 +0530

    backend api test

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -4,20 +4,20 @@ import { delay, Observable, of, throwError } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private api = 'http://localhost:8080/api/auth';
+  private api = 'https://agro-backend-ms-f2dechbuaggncfdf.centralindia-01.azurewebsites.net'
 
   constructor(private http: HttpClient) {}
 
   login(mobile: string, password: string): Observable<any> {
-    //return this.http.post(`${this.api}/login`, { mobile, password });
-    if (mobile === '9999999999' && password === '1234') {
-      const mockResponse = {
-        id: 1,
-        name: 'Ram Prasad'
-      };
-      return of(mockResponse).pipe(delay(1000)); // Simulate 1s delay
-    } else {
-      return throwError(() => new Error('Invalid credentials')).pipe(delay(500));
-    }
+    return this.http.post(`${this.api}/api/farmer/login`, { mobile, password });
+    // if (mobile === '9999999999' && password === '1234') {
+    //   const mockResponse = {
+    //     id: 1,
+    //     name: 'Ram Prasad'
+    //   };
+    //   return of(mockResponse).pipe(delay(1000)); // Simulate 1s delay
+    // } else {
+    //   return throwError(() => new Error('Invalid credentials')).pipe(delay(500));
+    // }
   }
 }
